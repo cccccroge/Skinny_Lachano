c28915487aa18cb48619d99856c567b70a8f98fd
cccccroge <pooopooo543@gmail.com>
2021-06-17 23:00:56 +0800

    Refactor lachano controller

--- game/characters/Lachano/Lachano.gd
+++ game/characters/Lachano/Lachano.gd
@@ -1,87 +1,66 @@
 extends KinematicBody2D
 
-# Nodes
-onready var anim = get_node("AnimatedSprite")
-
-# External Variables
-export(int) var BASE_SPEED
 export(int) var SPEED
+export(float) var ANIM_SPEED_RATIO
 
-# Operational Variables
-var dir = Vector2()
-var vel = Vector2()
-var key_rec = { "DOWN":false, "UP":false, "LEFT":false, "RIGHT":false }
+onready var anim := get_node("AnimatedSprite")
+onready var ANIM_SPEED = SPEED * ANIM_SPEED_RATIO
+var key_rec := {}
 
-# Overriding Functions
+func _init():
+	for action in InputMap.get_actions():
+		if action.find("char_") == 0:
+			key_rec[action] = false
+
 func _ready():
-	# init animation
-	change_animation_to("front_idle")
-	anim.flip_h = false
-	# init speed
-	change_moving_speed(SPEED)
-	
-	set_physics_process(true)
-	set_process_input(true)
+	change_animation_to("front_idle", Vector2())
+	change_anim_speed(ANIM_SPEED)
 
 func _input(event):
-	if event.is_action_type():
-		if event.is_action_pressed("Down"):
-			key_rec["DOWN"] = true
-		elif event.is_action_released("Down"):
-			key_rec["DOWN"] = false
-		if event.is_action_pressed("Up"):
-			key_rec["UP"] = true
-		elif event.is_action_released("Up"):
-			key_rec["UP"] = false
-		if event.is_action_pressed("Left"):
-			key_rec["LEFT"] = true
-		elif event.is_action_released("Left"):
-			key_rec["LEFT"] = false
-		if event.is_action_pressed("Right"):
-			key_rec["RIGHT"] = true
-		elif event.is_action_released("Right"):
-			key_rec["RIGHT"] = false
+	if !event.is_action_type():
+		return
+	for key in key_rec:
+		if event.is_action_pressed(key):
+			key_rec[key] = true
+		elif event.is_action_released(key):
+			key_rec[key] = false 
 
-func _physics_process(delta):
-	# decide the vel base on input
-	dir = Vector2(0, 0)
-	if key_rec["UP"] == true:
-		dir.y -= 1
-	if key_rec["DOWN"] == true:
-		dir.y += 1
-	if key_rec["LEFT"] == true:
-		dir.x -= 1
-	if key_rec["RIGHT"] == true:
-		dir.x += 1 
-	vel = dir.normalized() * SPEED
-	
-	# apply movement to character
+func _physics_process(_delta):
+	var vel := cal_velocity()
 	move_and_slide(vel)
-	
-	# change animation by velocity
-	if vel.length() == 0 and anim.animation != "front_idle":
-		change_animation_to("front_idle")
-		anim.flip_h = false
-	elif vel.length() != 0:
+	change_animation_by_vel(vel)
+
+func cal_velocity() -> Vector2:
+	var dir := Vector2(0, 0)
+	if key_rec["char_up"]:
+		dir.y -= 1
+	if key_rec["char_down"]:
+		dir.y += 1
+	if key_rec["char_left"]:
+		dir.x -= 1
+	if key_rec["char_right"]:
+		dir.x += 1
+	return dir.normalized() * SPEED
+
+func change_animation_by_vel(vel):
+	if vel.length() == 0:
+		if anim.animation != "front_idle":
+			change_animation_to("front_idle", vel)
+	else:
 		if vel.x > 0 and vel.y == 0:
-			change_animation_to("side_move")
-			anim.flip_h = false
+			change_animation_to("side_move", vel)
 		elif vel.x < 0 and vel.y == 0:
-			change_animation_to("side_move")
-			anim.flip_h = true
+			change_animation_to("side_move", vel)
 		elif vel.y > 0 and vel.x == 0:
-			change_animation_to("front_move")
-			anim.flip_h = false
+			change_animation_to("front_move", vel)
 		elif vel.y < 0 and vel.x == 0:
-			change_animation_to("behind_move")
-			anim.flip_h = false
+			change_animation_to("behind_move", vel)
 
-# Custom Functions
-func change_animation_to(name):
-	anim.play(name)
+func change_animation_to(anim_name, velocity):
+	anim.play(anim_name)
+	anim.flip_h = (velocity.x < 0 and velocity.y == 0)
 
-func change_moving_speed(speed):
-	anim.frames.set_animation_speed("side_move", SPEED / BASE_SPEED * 60)
-	anim.frames.set_animation_speed("front_move", SPEED / BASE_SPEED * 60)
-	anim.frames.set_animation_speed("behind_move", SPEED / BASE_SPEED * 60)
-
+func change_anim_speed(speed):
+	anim.frames.set_animation_speed("side_move", speed)
+	anim.frames.set_animation_speed("front_move", speed)
+	anim.frames.set_animation_speed("behind_move", speed)
