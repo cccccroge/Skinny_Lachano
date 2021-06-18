a3bf25fff09dc0122ceabc77082b3fa9cc4a89e9
cccccroge <pooopooo543@gmail.com>
2021-06-18 20:06:12 +0800

    Refactor lachano controller.
    1. create more functions
    2. fix input handling callback from _input to _unhandled_input

--- game/characters/Lachano/Lachano.gd
+++ game/characters/Lachano/Lachano.gd
@@ -8,27 +8,42 @@ onready var ANIM_SPEED = SPEED * ANIM_SPEED_RATIO
 var key_rec := {}
 
 func _init():
-	for action in InputMap.get_actions():
-		if action.find("char_") == 0:
-			key_rec[action] = false
+	create_key_records()
 
 func _ready():
 	change_animation_to("front_idle", Vector2())
 	change_anim_speed(ANIM_SPEED)
 
-func _input(event):
+func _unhandled_input(event):
 	if !event.is_action_type():
 		return
-	for key in key_rec:
-		if event.is_action_pressed(key):
-			key_rec[key] = true
-		elif event.is_action_released(key):
-			key_rec[key] = false 
+	record_key_records(event)
 
 func _physics_process(_delta):
 	var vel := cal_velocity()
 	move_and_slide(vel)
 	change_animation_by_vel(vel)
+
+func create_key_records():
+	for action in InputMap.get_actions():
+		if action.find("char_") == 0:
+			key_rec[action] = false
+
+func record_key_records(event):
+	for key in key_rec:
+		if event.is_action_pressed(key):
+			key_rec[key] = true
+		elif event.is_action_released(key):
+			key_rec[key] = false
+
+func change_animation_to(anim_name, velocity):
+	anim.play(anim_name)
+	anim.flip_h = (velocity.x < 0 and velocity.y == 0)
+
+func change_anim_speed(speed):
+	anim.frames.set_animation_speed("side_move", speed)
+	anim.frames.set_animation_speed("front_move", speed)
+	anim.frames.set_animation_speed("behind_move", speed)
 
 func cal_velocity() -> Vector2:
 	var dir := Vector2(0, 0)
@@ -55,12 +70,3 @@ func change_animation_by_vel(vel):
 			change_animation_to("front_move", vel)
 		elif vel.y < 0 and vel.x == 0:
 			change_animation_to("behind_move", vel)
-
-func change_animation_to(anim_name, velocity):
-	anim.play(anim_name)
-	anim.flip_h = (velocity.x < 0 and velocity.y == 0)
-
-func change_anim_speed(speed):
-	anim.frames.set_animation_speed("side_move", speed)
-	anim.frames.set_animation_speed("front_move", speed)
-	anim.frames.set_animation_speed("behind_move", speed)
