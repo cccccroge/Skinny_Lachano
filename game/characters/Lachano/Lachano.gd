21bc0e394c65184213d2c2eda8057ef3b6a6a147
cccccroge <pooopooo543@gmail.com>
2021-06-19 21:48:45 +0800

    Add main statuses and testing scene 1. create GameplayTesting scene 2. add assisting ui to control values 3. add auto healing

--- game/characters/Lachano/Lachano.gd
+++ game/characters/Lachano/Lachano.gd
@@ -3,25 +3,29 @@ extends KinematicBody2D
 export(int) var SPEED
 export(float) var ANIM_SPEED_RATIO
 
-onready var anim := get_node("AnimatedSprite")
+onready var anim := $AnimatedSprite
+onready var statuses := $MainStatuses
 onready var ANIM_SPEED = SPEED * ANIM_SPEED_RATIO
 var key_rec := {}
+var vel := Vector2()
 
 func _init():
 	create_key_records()
 
 func _ready():
-	change_animation_to("front_idle", Vector2())
+	change_animation_to("front_idle", vel)
 	change_anim_speed(ANIM_SPEED)
-
+	
 func _unhandled_input(event):
 	if !event.is_action_type():
 		return
 	record_key_records(event)
 
 func _physics_process(_delta):
-	var vel := cal_velocity()
-	move_and_slide(vel)
+	var prev_vel := vel
+	vel = cal_velocity()
+	trigger_healing_mech(vel, prev_vel)
+	var _vel := move_and_slide(vel)
 	change_animation_by_vel(vel)
 
 func create_key_records():
@@ -29,18 +33,18 @@ func create_key_records():
 		if action.find("char_") == 0:
 			key_rec[action] = false
 
-func record_key_records(event):
+func record_key_records(event: InputEvent):
 	for key in key_rec:
 		if event.is_action_pressed(key):
 			key_rec[key] = true
 		elif event.is_action_released(key):
 			key_rec[key] = false
 
-func change_animation_to(anim_name, velocity):
+func change_animation_to(anim_name: String, velocity: Vector2):
 	anim.play(anim_name)
 	anim.flip_h = (velocity.x < 0 and velocity.y == 0)
 
-func change_anim_speed(speed):
+func change_anim_speed(speed: float):
 	anim.frames.set_animation_speed("side_move", speed)
 	anim.frames.set_animation_speed("front_move", speed)
 	anim.frames.set_animation_speed("behind_move", speed)
@@ -57,16 +61,23 @@ func cal_velocity() -> Vector2:
 		dir.x += 1
 	return dir.normalized() * SPEED
 
-func change_animation_by_vel(vel):
-	if vel.length() == 0:
+func trigger_healing_mech(velocity: Vector2, prev_vel: Vector2):
+	if prev_vel.length() != 0 and velocity.length() == 0:
+		statuses.start_heal_timer()
+	elif prev_vel.length() == 0 and velocity.length() != 0:
+		statuses.stop_heal_timer()
+		statuses.stop_healing()
+
+func change_animation_by_vel(velocity: Vector2):
+	if velocity.length() == 0:
 		if anim.animation != "front_idle":
-			change_animation_to("front_idle", vel)
+			change_animation_to("front_idle", velocity)
 	else:
-		if vel.x > 0 and vel.y == 0:
-			change_animation_to("side_move", vel)
-		elif vel.x < 0 and vel.y == 0:
-			change_animation_to("side_move", vel)
-		elif vel.y > 0 and vel.x == 0:
-			change_animation_to("front_move", vel)
-		elif vel.y < 0 and vel.x == 0:
-			change_animation_to("behind_move", vel)
+		if velocity.x > 0 and velocity.y == 0:
+			change_animation_to("side_move", velocity)
+		elif velocity.x < 0 and velocity.y == 0:
+			change_animation_to("side_move", velocity)
+		elif velocity.y > 0 and velocity.x == 0:
+			change_animation_to("front_move", velocity)
+		elif velocity.y < 0 and velocity.x == 0:
+			change_animation_to("behind_move", velocity)
---
+++ game/sandbox/GameplayTesting.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+onready var health_progress_bar := $UiLayer/VBoxContainer2/HBoxContainer/HealthBar
+onready var bone_progress_bar := $UiLayer/VBoxContainer2/HBoxContainer2/BoneBar
+onready var intestinal_progress_bar := $UiLayer/VBoxContainer2/HBoxContainer3/IntestinalBar
+onready var decre_health_btn := $UiLayer/VBoxContainer/DecreHealthBtn
+onready var incre_health_btn := $UiLayer/VBoxContainer/IncreHealthBtn
+onready var lachano_status := $SceneLayer/Obstacles/Lachano/MainStatuses
+
+func _ready():
+	init_status_ui()
+	connect_signals()
+
+func init_status_ui():
+	health_progress_bar.min_value = 0
+	health_progress_bar.max_value = lachano_status.max_status["health"]
+	bone_progress_bar.min_value = 0
+	bone_progress_bar.max_value = lachano_status.max_status["bone"]
+	intestinal_progress_bar.min_value = 0
+	intestinal_progress_bar.max_value = \
+		lachano_status.max_status["intestinal_juice"]
+	update_progress_bars()
+
+func connect_signals():
+	var _err
+	_err = decre_health_btn.connect("button_down", \
+		lachano_status, "alter_status_by", ["health", -5])
+	_err = incre_health_btn.connect("button_down", \
+		lachano_status, "alter_status_by", ["health", 5])
+	_err = lachano_status.connect("status_altered", \
+		self, "update_progress_bars")
+
+func update_progress_bars():
+	health_progress_bar.value = lachano_status.status["health"]
+	bone_progress_bar.value = lachano_status.status["bone"]
+	intestinal_progress_bar.value = lachano_status.status["intestinal_juice"]
